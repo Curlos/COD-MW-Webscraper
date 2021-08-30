4609ff87a3807a637cfed1f3217825e9c0fdc4ea
Carlos Martinez <curlosmart@gmail.com>
2021-08-30 10:18:52 -0400

    get one weapon detailed data

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import shutil
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import json
+from pprint import pprint
 
 chrome_driver_path = "/Users/curlos/Desktop/Development/chromedriver"
 
@@ -43,6 +45,66 @@ def get_all_weapons(url):
         i += 1
 
 
+def get_one_weapon_data(url):
+    weapons = {'Assault Rifles': [], 'Sub Machine Guns': [], 'Shotguns': [], 'Light Machine Guns': [
+    ], 'Marksman Rifles': [], 'Sniper Rifles': [], 'Handguns': [], 'Rocket Launchers': [], 'Melee': []}
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(
+        options=options, executable_path=chrome_driver_path)
+
+    driver.get(url)
+    weapon_name = driver.find_element_by_css_selector(
+        'h1.contentheading').get_attribute("textContent").strip()
+
+    weapon_class = driver.find_element_by_css_selector(
+        'a[title^="Weapon Class"]').get_attribute("textContent").strip()
+
+    weapon_accuracy = int(driver.find_element_by_css_selector(
+        '.field-entry.accuracy .field-value').get_attribute("textContent").strip())
+
+    weapon_damage = int(driver.find_element_by_css_selector(
+        '.field-entry.damage .field-value').get_attribute("textContent").strip())
+
+    range_mobility_control = driver.find_elements_by_css_selector(
+        '.field-entry.range .field-value')
+
+    weapon_range = int(
+        range_mobility_control[0].get_attribute("textContent").strip())
+
+    weapon_fire_rate = int(driver.find_element_by_css_selector(
+        '.field-entry.fire-rate .field-value').get_attribute("textContent").strip())
+
+    weapon_mobility = int(
+        range_mobility_control[1].get_attribute("textContent").strip())
+
+    weapon_control = int(
+        range_mobility_control[2].get_attribute("textContent").strip())
+
+    weapon_overall = (weapon_accuracy + weapon_damage + weapon_range +
+                      weapon_fire_rate + weapon_mobility + weapon_control) / 6
+
+    rounded_overall = round(weapon_overall, 2)
+
+    weapon_description = driver.find_element_by_css_selector(
+        '.article-content p').get_attribute("textContent").strip()
+
+    weapons[weapon_class].append({
+        'name': weapon_name,
+        'class': weapon_class,
+        'accuracy': int(weapon_accuracy),
+        'damage': int(weapon_damage),
+        'range': int(weapon_range),
+        'fire_rate': int(weapon_fire_rate),
+        'mobility': int(weapon_mobility),
+        'control': int(weapon_control),
+        'overall': rounded_overall,
+        'description': weapon_description
+    })
+
+    pprint(weapons)
+
+
 def get_one_file(filename, img_link, save_dir):
 
     print(f"Downloading {filename}...", end="")
@@ -61,7 +123,9 @@ def get_one_file(filename, img_link, save_dir):
         print(f"Failed!")
 
 
-get_all_killstreaks(
-    'https://blog.activision.com/call-of-duty/2019-10/The-Basics-of-Call-of-Duty-Modern-Warfare-Killstreaks')
+# get_all_killstreaks('https://blog.activision.com/call-of-duty/2019-10/The-Basics-of-Call-of-Duty-Modern-Warfare-Killstreaks')
 
-get_all_weapons('https://www.gamesatlas.com/cod-modern-warfare/weapons/')
+# get_all_weapons('https://www.gamesatlas.com/cod-modern-warfare/weapons/')
+
+get_one_weapon_data(
+    'https://www.gamesatlas.com/cod-modern-warfare/weapons/combat-knife')
